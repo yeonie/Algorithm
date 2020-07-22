4205a181c4dc8bf0d624b6f480d754afb6e2a700
litong <ben2740@naver.com>
2020-07-22 15:38:59 +0900

    sort and overlap parameter rm

---
+++ b_2750/b_2750/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  b_2750
+//
+//  Created by 이동연 on 22/07/2020.
+//  Copyright © 2020 litong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
